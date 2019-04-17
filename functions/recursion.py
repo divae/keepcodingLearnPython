461832f0195af77b3b44d0f27a03c49c980b26f4
Estela Medrano <stlmedrano@gmail.com>
2019-04-17 18:22:24 +0200

    recursividad

--- functions/recursion.py
+++ functions/recursion.py
@@ -1,9 +1,17 @@
 entrada = 10
 
-def counter(e):
+def retrocontador(e):
     print("{},".format(e),end=" ")
     if e > 0:
-        counter(e-1)
+        retrocontador(e-1)
 
-counter(entrada)
+retrocontador(entrada)
 
+def sumatorio(e):
+    if e > 0:
+        return e + sumatorio(e-1)
+    else:
+        return 0
+
+print(sumatorio(4))
+    
