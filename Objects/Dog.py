fb5ae395ab7f830cfc55ae18e80ed3e17ffd230d
Estela Medrano <stlmedrano@gmail.com>
2019-04-17 19:38:29 +0200

    objects

--- Objects/Dog.py
+++ Objects/Dog.py
@@ -1,18 +1,54 @@
 
 class Dog():
     def __init__(self,name,year,weight):
-        self.name = name
-        self.year = year
-        self.weight = weight
+        self.__name = name
+        self.__year = year
+        self.__weight = weight
 
     def bark(self):
         print("Guau Guau")
+
+    def name(self):
+        return self.__name
+    
+    def year(self):
+        return self.__year
+
+    def weight(self):
+        return self.__weight
+
+    def __str__(self):
+        return "I'm {}, year:{}, weight:{}".format(self.__name,self.__year,self.__weight)
+
+class AssistantDog(Dog):
+    def __init__(self, name, year, weight, owner):
+        Dog.__init__(self, name, year, weight)
+        self.__owner = owner
+        self.working = False
+
+    def bark(self):
+        if self.working:
+            print("Shhh, I can't bark")
+        else:
+            Dog.bark(self)
     
     def __str__(self):
-        return "I'm {}, year:{}, weight:{}".format(self.name,self.year,self.weight)
+        return "Dog {}, year:{}, weight:{},Assistan of owner:{}".format(self.name(),self.year(),self.weight(),self.__owner)
+
+
+
+
 
 
 dog = Dog('Sora',2,10)
 
 dog.bark()
 print(dog)
+
+assistandog = AssistantDog('Sora',2,10,'Estela')
+
+assistandog.bark()
+assistandog.working = True
+assistandog.bark()
+
+print(assistandog)
